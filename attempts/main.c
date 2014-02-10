13bd36e7a0dbdb8ecc6e3218db1382f559fe7d71
Christian Gutierrez <chesstrian@gmail.com>
2014-02-10 16:04:28 -0500

    Initial eclipse setup.

--- attempts/main.c
+++ attempts/main.c
@@ -13,18 +13,18 @@ int main(int argc, char const *argv[]) {
     int result = get_file_content(argv[1], &buffer);
 
     switch (result) {
-      case FILE_CONTENT_READ:
-        u = str_new(buffer);
-        str_print(u);
-        break;
-      case FILE_CONTENT_UNREAD:
-        fprintf(stderr, "Could not read file's content.\n");
-        break;
-      case CANNOT_OPEN_FILE:
-        fprintf(stderr, "Could not open file.\n");
-        break;
-      default:
-        break;
+    case FILE_CONTENT_READ:
+      u = str_new(buffer);
+      str_print(u);
+      break;
+    case FILE_CONTENT_UNREAD:
+      fprintf(stderr, "Could not read file's content.\n");
+      break;
+    case CANNOT_OPEN_FILE:
+      fprintf(stderr, "Could not open file.\n");
+      break;
+    default:
+      break;
     }
   } else {
     fprintf(stderr, "Usage: %s file.\n", argv[0]);
