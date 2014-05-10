1e01efaf4c642e014474e3c5f99a6873d812769f
Christian Gutierrez <chesstrian@gmail.com>
2014-05-10 13:19:39 -0500

    Fixes bounds.

--- fgb.bash
+++ fgb.bash
@@ -7,7 +7,7 @@ primes=( 2 3 5 7 11 13 17 19 23 29 31 37 )
 
 for q in "${primes[@]}"
 do
-  for n in $(seq 2 13)
+  for n in $(seq 2 12)
   do
     $EXE $DIR"/pols/pol_q"$q"_n"$n".txt" $n $q 1
   done
